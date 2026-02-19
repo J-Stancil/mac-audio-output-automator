c7320547589b8f106f338ce60541f68fe19b59f5
J.Stan <stancil.j@gmail.com>
2026-02-19 05:32:51 -0500

    Initial Commit

---
+++ AudioSwitcher/AudioSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSwitcherApp.swift
+//  AudioSwitcher
+//
+//  Created by Jason H Stancil on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
