240c9828635b9986765e25f0dca7d553ee0ec047
Jason <stancil.j@gmail.com>
2026-03-22 00:37:08 -0400

    Initial Commit

---
+++ MenuBarAudio/MenuBarAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarAudioApp.swift
+//  MenuBarAudio
+//
+//  Created by Jason H Stancil on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
